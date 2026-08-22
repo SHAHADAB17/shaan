3a4990fb9d7ef42c7f125035b31e6148be1f81ec
Shahadab sheikh <shahadabsheikh13@gmail.com>
2026-08-22 13:10:25 +0530

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,8 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+const config: CapacitorConfig={
+  appId:'com.shaan.chat',
+  appName:'ShaanChat',
+  webDir:'www',
+  android:{allowMixedContent:false}
+};
+export default config;
